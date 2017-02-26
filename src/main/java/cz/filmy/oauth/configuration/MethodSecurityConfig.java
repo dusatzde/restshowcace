9dde6739a47191c1b84e37cb73958f6e468a53c3
zdusatko <zdusatko@newps.cz>
2017-02-26 21:14:34 +0100

    cors

--- src/main/java/cz/filmy/oauth/configuration/MethodSecurityConfig.java
+++ src/main/java/cz/filmy/oauth/configuration/MethodSecurityConfig.java
@@ -1,12 +1,16 @@
 package cz.filmy.oauth.configuration;
 
+import io.swagger.models.HttpMethod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.method.OAuth2MethodSecurityConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 /**
  * Created by Zdenek on 09-Dec-16.
